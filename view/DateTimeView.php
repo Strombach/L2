fbc1ab86305a78b2b1a568c01752089bf7fd0192
Fredrik <fredde.stromback@gmail.com>
2019-09-13 14:12:20 +0200

    Adds the time string to the login view.

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,12 @@ class DateTimeView {
 
 
 	public function show() {
+    $dayText = date('l');
+    $dateNum = date('jS');
+    $monthAndYear = date ('F Y');
+    $time = date('G:i:s');
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = "$dayText, the $dateNum of $monthAndYear, The time is $time ";
 
 		return '<p>' . $timeString . '</p>';
 	}
